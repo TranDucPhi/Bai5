fed26f1573c3e0381e0835eb4c5c4cc022ee72e6
MAY 06 <MAY 06@LAB2-33>
2023-09-12 08:26:52 +0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cc As Integer
+        cc = Integer.Parse(txtcc.Text)
+        Dim i As Integer
+        For i = 1 To 10
+            Dim kq As String
+            kq = i & " x " & cc & " = " & i * cc
+            ListBox1.Items.Add(kq)
+        Next
+    End Sub
+End Class
